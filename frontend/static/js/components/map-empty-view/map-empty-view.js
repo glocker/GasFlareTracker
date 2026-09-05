e2dc4a84dd6d63a70a23f300d96a0c8469196854
glocker <georgeglocker@gmail.com>
2026-09-05 18:22:27 +0700

    Update empty view

--- frontend/static/js/components/map-empty-view/map-empty-view.js
+++ frontend/static/js/components/map-empty-view/map-empty-view.js
@@ -4,7 +4,7 @@ import { RegionFilter } from "#app/components/region-filter/region-filter.js";
 /**
  * Empty view for facility map
  */
-export class MapEmptyState {
+export class MapEmptyView {
   /** @type {HTMLElement} */
   element;
 
@@ -23,19 +23,19 @@ export class MapEmptyState {
     this.regionFilter = new RegionFilter(onRegionChange);
 
     const message = document.createElement("p");
-    message.className = "map-empty-state__message";
+    message.className = "map-empty-view__message";
     message.textContent = "Oops, no data in chosen period or region. Pick another one.";
 
     const filters = document.createElement("div");
-    filters.className = "map-empty-state__filters";
+    filters.className = "map-empty-view__filters";
     filters.append(this.periodFilter.input, this.regionFilter.select);
 
     const panel = document.createElement("div");
-    panel.className = "map-empty-state__panel";
+    panel.className = "map-empty-view__panel";
     panel.append(message, filters);
 
     this.element = document.createElement("div");
-    this.element.className = "map-empty-state";
+    this.element.className = "map-empty-view";
     // Hidden until facility-map decides otherwise - not shown/hidden by any
     // logic in here
     this.element.hidden = true;
